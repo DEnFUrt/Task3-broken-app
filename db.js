2d0bf39866701c59909ce7443dd6ad141900c533
DEnFUrt <musorov@gmail.com>
2021-05-19 00:34:33 +0500

    feat: add dotenv for module db.js

--- db.js
+++ db.js
@@ -1,7 +1,19 @@
 const Sequelize = require('sequelize');
+const dotenv = require('dotenv');
+const path = require('path');
+
+dotenv.config({
+    path: path.join(__dirname, '/.env')
+});
+
+const username = process.env.DB_USER;
+const password = process.env.DB_PASSWORD;
+const host = process.env.DB_HOST;
+const database = process.env.DB;
+
                                 //database username   password
-const sequelize = new Sequelize('gamedb', 'postgres', 'ghastb0i', {
-    host: 'localhost',
+const sequelize = new Sequelize(database, username, password, {
+    host: host,
     dialect: 'postgres'
 })
 
