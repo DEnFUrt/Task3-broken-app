1a979a09c94a6cb04e46cc83dcdf8c740de3ab95
DEnFUrt <musorov@gmail.com>
2021-05-22 00:33:46 +0500

    fix: logical errors on module gamecontroller.js

--- app.js
+++ app.js
@@ -8,15 +8,6 @@ db.sync();
 
 app.use(require('body-parser').json()); 
 
-// Проверка работы сервера, убрать потом
-app.use('/', (req, res, next) => {
-    if (req.originalUrl === '/') {
-        res.send('Service is running!');
-        return;
-    }
-    next();
-});
-
 app.use('/api/auth', user);
 app.use(require('./middleware/validate-session'))
 app.use('/api/game', game);
